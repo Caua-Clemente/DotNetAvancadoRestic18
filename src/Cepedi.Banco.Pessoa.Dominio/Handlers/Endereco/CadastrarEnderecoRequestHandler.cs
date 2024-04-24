a69cbc347fa405915052b18f65977514a8d7a620
Caua-Clemente <cauaclementelima@gmail.com>
2024-04-23 22:14:25 -0300

    Progresso na verificacao de cep

--- src/Cepedi.Banco.Pessoa.Dominio/Handlers/Endereco/CadastrarEnderecoRequestHandler.cs
+++ src/Cepedi.Banco.Pessoa.Dominio/Handlers/Endereco/CadastrarEnderecoRequestHandler.cs
@@ -2,6 +2,8 @@
 using Cepedi.Banco.Pessoa.Compartilhado.Responses;
 using Cepedi.Banco.Pessoa.Dominio.Entidades;
 using Cepedi.Banco.Pessoa.Dominio.Repository;
+using Cepedi.Banco.Pessoa.Dominio.Services;
+
 using MediatR;
 using Microsoft.Extensions.Logging;
 using OperationResult;
@@ -33,6 +35,22 @@ public class CadastrarEnderecoRequestHandler : IRequestHandler<CadastrarEndereco
         };
 
         //Verificação de CEP
+        ViaCEPService _ViaCEPService = new ViaCEPService();
+        
+        var resultCEP = await _ViaCEPService.GetCEP(endereco.Cep);
+        if(!resultCEP)
+        {
+            return Result.BadRequest();
+        }
+
+        /*
+            Não está funcionando. Tentei facilitar a checagem do resultado do 
+            GetCEP substituindo o retorno de um objeto por um retorno de bool,
+            ainda assim, não consegui fazer algo do tipo "BadRequest" ou "NoContent"
+            funcionar aqui. No "ViaCEPService, deixei comentado o código original
+            (o que retornaria um objeto ou uma mensagem ou um exception), mas também
+            não tenho segurança de que ele esteja estruturado corretamente.
+        */
 
         await _enderecoRepository.CadastrarEnderecoAsync(endereco);
 
--- src/Cepedi.Banco.Pessoa.Dominio/Servicos/ViaCEPService.cs
+++ src/Cepedi.Banco.Pessoa.Dominio/Servicos/ViaCEPService.cs
@@ -4,43 +4,47 @@ using System.Text.Json;
 namespace Cepedi.Banco.Pessoa.Dominio.Services;
 
 public class ResponseViaCEP{
-  public string Cep;
-  public string Logradouro;
-  public string Complemento;
-  public string Bairro;
-  public string Localidade;
-  public string Uf;
-  public string Ibge;
-  public string Gia;
-  public string Ddd;
-  public string Siafi;
+  public string? Cep;
+  public string? Logradouro;
+  public string? Complemento;
+  public string? Bairro;
+  public string? Localidade;
+  public string? Uf;
+  public string? Ibge;
+  public string? Gia;
+  public string? Ddd;
+  public string? Siafi;
+  public string? Error;
 }
 
 public class ViaCEPService
 {
-    public async Task<object> GetCEP()
+    // public async Task<object> GetCEP(string cep)
+    public async Task<bool> GetCEP(string cep)
     {
-        string url = "https://viacep.com.br/ws/45860000/json/";
+        string url = $"https://viacep.com.br/ws/{cep}/json/";
 
         var httpClient = new HttpClient();
-
+    
         var response = await httpClient.GetAsync(url);
 
         if(!response.IsSuccessStatusCode)
         {
-            if(response.StatusCode == HttpStatusCode.BadRequest)
-            {
-                var responseContentError = await response.Content.ReadAsStringAsync();
-                return JsonSerializer.Deserialize<ResponseViaCEP>(responseContentError);
-            }
-            else
-            {
-                throw new Exception($"Error: {response.StatusCode} - {response.ReasonPhrase}");
-            }
+            // if(response.StatusCode == HttpStatusCode.BadRequest)
+            // {
+            //     var responseContentError = await response.Content.ReadAsStringAsync();
+            //     return JsonSerializer.Deserialize<ResponseViaCEP>(responseContentError);
+            // }
+            // else
+            // {
+            //     throw new Exception($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+            // }
+            return false;
         }
 
-        var responseContent = await response.Content.ReadAsStringAsync();
-        var BulkViaCEPResponse = JsonSerializer.Deserialize<ResponseViaCEP>(responseContent);
-        return BulkViaCEPResponse;
+        // var responseContent = await response.Content.ReadAsStringAsync();
+        // var BulkViaCEPResponse = JsonSerializer.Deserialize<ResponseViaCEP>(responseContent);
+        // return BulkViaCEPResponse;
+        return true;
     }
 }
